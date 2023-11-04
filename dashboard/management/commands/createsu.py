139bdcb48c5560777e5e57601c6cc1ebb4facf7c
Ben Siver <bsiver@gmail.com>
2023-11-04 14:35:32 -0500

    Recreate su

--- dashboard/management/commands/createsu.py
+++ dashboard/management/commands/createsu.py
@@ -8,8 +8,8 @@ class Command(BaseCommand):
     help = 'Creates a superuser.'
 
     def handle(self, *args, **options):
-        User.objects.filter(username='admin').delete()
-        username = os.getenv("SU_USERNAME"),
+        username = os.getenv("SU_USERNAME")
         password = os.getenv("SU_PASSWORD")
+        User.objects.filter(username=username).delete()
         print(f'Creating superuser | {username} | {password}')
         User.objects.create_superuser(username=username, password=password)
